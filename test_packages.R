de62911308e79001ae6bd993d2b3fae6fff20802
Ande8056 <Ande8056@gmail.com>
2024-12-16 22:32:11 +0100

    Update test_packages.R

--- test_packages.R
+++ test_packages.R
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 # Ready for the exam tomorrow!
 library(tidyverse)
-# Why is this not on my Github??
-=======
-# Trying to figure out why this doesn't update..
->>>>>>> 283450ad00bc62af81aab4f0c7d9ffa2f76c66ef
